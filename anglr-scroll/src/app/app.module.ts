df7197da83fc96059e27dd7169346cb40e715d5a
Andor <andor.auber@gmail.com>
2021-03-20 15:13:10 +0100

    feat: initialize angular project

---
+++ anglr-scroll/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContainerComponent } from './container/container.component';
+import { CardComponent } from './card/card.component';
+import { ContainerScrollComponent } from './container-scroll/container-scroll.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContainerComponent,
+    CardComponent,
+    ContainerScrollComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ anglr-scroll/src/app/container-scroll/container-scroll.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container-scroll',
+  templateUrl: './container-scroll.component.html',
+  styleUrls: ['./container-scroll.component.scss']
+})
+export class ContainerScrollComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
